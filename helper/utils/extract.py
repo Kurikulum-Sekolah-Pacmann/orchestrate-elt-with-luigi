d15040c206a92a7e239078bdc34a5fe7f3dddbfe
rahilaode <laoderahilatansa@gmail.com>
2024-03-17 23:46:27 +0700

    modify to extract all tables without specifying in luigi build

--- helper/utils/extract.py
+++ helper/utils/extract.py
@@ -9,26 +9,30 @@ load_dotenv()
 EXTRACT_OUTPUT_DIR = os.getenv("EXTRACT_OUTPUT_DIR")
 
 class Extract(luigi.Task):
-    table_name = luigi.Parameter()
+    tables_to_extract = ['category', 'subcategory', 'customer', 'orders', 'order_detail', 'product']  # Define tables to extract
 
     def output(self):
-        return luigi.LocalTarget(f'{EXTRACT_OUTPUT_DIR}{self.table_name}.csv')
+        outputs = []
+        for table_name in self.tables_to_extract:
+            outputs.append(luigi.LocalTarget(f'{EXTRACT_OUTPUT_DIR}/{table_name}.csv'))
+        return outputs
 
     def run(self):
         try:
             # Connect to PostgreSQL database
             conn_src, cur_src, conn_dwh, cur_dwh = db_connection()
 
-            # Query to select data from the table
-            query = f"SELECT * FROM {self.table_name}"
+            for table_name in self.tables_to_extract:
+                # Query to select data from the table
+                query = f"SELECT * FROM {table_name}"
 
-            # Read data into DataFrame
-            df = pd.read_sql_query(query, conn_src)
+                # Read data into DataFrame
+                df = pd.read_sql_query(query, conn_src)
 
-            # Write DataFrame to CSV
-            df.to_csv(f"{EXTRACT_OUTPUT_DIR}{self.table_name}.csv", index = False)
+                # Write DataFrame to CSV
+                df.to_csv(f"{EXTRACT_OUTPUT_DIR}/{table_name}.csv", index=False)
 
-            print(f"Data from table '{self.table_name}' exported to '{self.table_name}.csv' successfully.")
+                print(f"Data from table '{table_name}' exported to '{table_name}.csv' successfully.")
 
         except (Exception, psycopg2.Error) as error:
             print("Error while connecting to PostgreSQL:", error)
@@ -39,11 +43,5 @@ class Extract(luigi.Task):
                 conn_src.close()
 
 if __name__ == '__main__':
-    # Run Extract task
-    luigi.build([Extract(table_name = 'category'),
-                 Extract(table_name = 'subcategory'),
-                 Extract(table_name = 'customer'),
-                 Extract(table_name = 'orders'),
-                 Extract(table_name = 'order_detail'),
-                 Extract(table_name = 'product'),
-                 Extract(table_name = 'subcategory')], local_scheduler = True)
+    # Run Extract
+    luigi.build([Extract()], local_scheduler=True)

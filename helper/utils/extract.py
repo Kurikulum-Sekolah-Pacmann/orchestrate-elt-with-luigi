6875efc8c647d4940e0c6b2fb175527c1beeb7ed
rahilaode <laoderahilatansa@gmail.com>
2024-03-17 21:15:43 +0700

    modify extract task using luigi

--- helper/utils/extract.py
+++ helper/utils/extract.py
@@ -1,82 +1,49 @@
+import luigi
+import psycopg2
+from dotenv import load_dotenv
+import os
 import pandas as pd
-from helper.utils.db_conn import db_connection
-import warnings
-warnings.filterwarnings('ignore')
+from db_conn import db_connection
 
+load_dotenv()
+EXTRACT_OUTPUT_DIR = os.getenv("EXTRACT_OUTPUT_DIR")
 
-def get_table_data(table_name, engine):
-    """
-    Retrieves data from a specified table in a SQL database and returns it as a pandas DataFrame.
+class Extract(luigi.Task):
+    table_name = luigi.Parameter()
 
-    Args:
-        table_name (str): The name of the table from which to retrieve data.
-        engine (sqlalchemy.engine.base.Engine): SQLAlchemy engine object connected to the database.
+    def output(self):
+        return luigi.LocalTarget(f'{EXTRACT_OUTPUT_DIR}{self.table_name}.csv')
 
-    Returns:
-        pandas.DataFrame: A DataFrame containing the data from the specified table.
+    def run(self):
+        try:
+            # Connect to PostgreSQL database
+            conn_src, cur_src, conn_dwh, cur_dwh = db_connection()
 
-    Notes:
-        Ensure that the engine provided is properly configured and connected to the database.
-        Handle exceptions properly in the calling code.
-        If an error occurs during data retrieval, an empty DataFrame will be returned.
-    """
-    try:
-        # Construct SQL query to select all data from the specified table
-        query = f"SELECT * FROM {table_name};"
+            # Query to select data from the table
+            query = f"SELECT * FROM {self.table_name}"
 
-        # Execute the query and read the results into a DataFrame
-        df = pd.read_sql(query, engine)
-        
-        return df
+            # Read data into DataFrame
+            df = pd.read_sql_query(query, conn_src)
 
-    except Exception as e:
-        # Print the error message if an exception occurs
-        print(f"Error: {e}")
-        
-        # Return an empty DataFrame in case of an error
-        return pd.DataFrame()
+            # Write DataFrame to CSV
+            df.to_csv(f"{EXTRACT_OUTPUT_DIR}{self.table_name}.csv", index = False)
 
-def extract():
-    """
-    Extract data from source and staging databases, compare the data,
-    and save the differences to CSV files.
+            print(f"Data from table '{self.table_name}' exported to '{self.table_name}.csv' successfully.")
 
-    Returns:
-    None
+        except (Exception, psycopg2.Error) as error:
+            print("Error while connecting to PostgreSQL:", error)
 
-    Raises:
-    Any exceptions raised by the underlying functions/db_connection,
-    get_table_data, compare, or pandas.DataFrame.to_csv methods.
-    """
-    try:
-        # Establish connections to source and DWH databases
-        conn_src, cur_src, conn_dwh, cur_dwh = db_connection()
-        
-        # Extract from data sources
-        src_category = get_table_data('category', conn_src)
-        src_customer = get_table_data('customer', conn_src)
-        src_order_detail = get_table_data('order_detail', conn_src)
-        src_orders = get_table_data('orders', conn_src)
-        src_product = get_table_data('product', conn_src)
-        src_subcategory = get_table_data('subcategory', conn_src)
-        
-        # Save to csv file
-        src_category.to_csv('/home/laode/pacmann/project/elt-with-python/helper/utils/temp_data/category.csv', index = False) 
-        src_subcategory.to_csv('/home/laode/pacmann/project/elt-with-python/helper/utils/temp_data/subcategory.csv', index = False) 
-        src_customer.to_csv('/home/laode/pacmann/project/elt-with-python/helper/utils/temp_data/customer.csv', index = False) 
-        src_orders.to_csv('/home/laode/pacmann/project/elt-with-python/helper/utils/temp_data/orders.csv', index = False) 
-        src_product.to_csv('/home/laode/pacmann/project/elt-with-python/helper/utils/temp_data/product.csv', index = False) 
-        src_order_detail.to_csv('/home/laode/pacmann/project/elt-with-python/helper/utils/temp_data/order_detail.csv', index = False) 
-        
-        # Close the cursor and connection
-        conn_src.close()
-        cur_src.close()
-        conn_dwh.close()
-        cur_dwh.close()
+        finally:
+            # Close database conn_src
+            if conn_src:
+                conn_src.close()
 
-    except Exception as e:
-        print(f"Error extracting and loading data: {e}")
-        
-# Execute the functions when the script is run
-if __name__ == "__main__":
-    extract()
+if __name__ == '__main__':
+    # Run Extract task
+    luigi.build([Extract(table_name = 'category'),
+                 Extract(table_name = 'subcategory'),
+                 Extract(table_name = 'customer'),
+                 Extract(table_name = 'orders'),
+                 Extract(table_name = 'order_detail'),
+                 Extract(table_name = 'product'),
+                 Extract(table_name = 'subcategory')], local_scheduler = True)
